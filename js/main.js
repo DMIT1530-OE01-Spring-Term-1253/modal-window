a6221c427d0223b02bfa650bd9303e33858864da
Lauren Olsen <laureno@U212A-CT-SURF.nait.ca>
2026-07-23 20:52:54 -0600

    upload demo content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const myModal = document.querySelector(".overlay");
+
+window.addEventListener("load", function () {
+    setTimeout(function open(event) {
+            myModal.style.display = "block";
+        }, 1000);
+});
+
+document.getElementById("close").addEventListener
+    ("click", function () {
+        myModal.style.display = "none";
+    });
